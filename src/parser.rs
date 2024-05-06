9920ad0600734cb6b724c9ebd2f7da777ada612e
CarlosDanielMaturano <carlosdanielmaturano@proton.me>
2024-05-06 09:07:29 -0300

    feat: Parser now parses variable definitions and multi-line statements

--- src/parser.rs
+++ src/parser.rs
@@ -1,5 +1,4 @@
 use crate::lexer::Token;
-use std::iter::Peekable;
 
 #[derive(Debug, PartialEq)]
 pub enum Operator {
@@ -10,47 +9,53 @@ pub enum Operator {
 }
 
 #[derive(Debug, PartialEq)]
-pub enum Object {
+pub enum Literal {
     Void,
-    List(Vec<Object>),
+    List(Vec<Literal>),
     Number(f64),
+    Symbol(String),
     BinaryOperator(Operator),
 }
 
-pub fn parse(tokens: Vec<Token>) -> Result<Object, String> {
+pub fn parse(tokens: Vec<Token>) -> Result<Vec<Literal>, String> {
     if tokens.is_empty() {
-        return Ok(Object::Void);
+        return Ok(vec![Literal::Void]);
     }
-    parse_tokens(&mut tokens.into_iter().peekable())
+    let mut tokens = tokens.into_iter().peekable();
+    let mut literals: Vec<Literal> = Vec::new();
+    while let Some(_) = tokens.peek() {
+        literals.push(parse_tokens(&mut tokens)?);
+        tokens.next();
+    }
+    return Ok(literals);
 }
 
-type PeekableTokens = Peekable<std::vec::IntoIter<Token>>;
-fn parse_tokens(tokens: &mut PeekableTokens) -> Result<Object, String> {
+type PeekableTokens = std::iter::Peekable<std::vec::IntoIter<Token>>;
+fn parse_tokens(tokens: &mut PeekableTokens) -> Result<Literal, String> {
     let head = tokens.next();
-
     let Some(Token::Lparen) = head else {
         return Err(format!("Error: Expected Lparen, found: {:?}", head));
     };
 
-    let mut objects: Vec<Object> = Vec::new();
+    let mut objects: Vec<Literal> = Vec::new();
 
     while let Some(token) = tokens.peek() {
         match token {
             Token::Symbol(s) => match s.as_str() {
-                "+" => objects.push(Object::BinaryOperator(Operator::Add)),
-                "-" => objects.push(Object::BinaryOperator(Operator::Subtract)),
-                "*" => objects.push(Object::BinaryOperator(Operator::Multiply)),
-                "/" => objects.push(Object::BinaryOperator(Operator::Divide)),
-                _ => todo!("Missing symbols"),
+                "+" => objects.push(Literal::BinaryOperator(Operator::Add)),
+                "-" => objects.push(Literal::BinaryOperator(Operator::Subtract)),
+                "*" => objects.push(Literal::BinaryOperator(Operator::Multiply)),
+                "/" => objects.push(Literal::BinaryOperator(Operator::Divide)),
+                _ => objects.push(Literal::Symbol(s.to_string())),
             },
-            Token::Number(n) => objects.push(Object::Number(*n)),
+            Token::Number(n) => objects.push(Literal::Number(*n)),
             Token::Lparen => objects.push(parse_tokens(tokens)?),
             Token::Rparen => break,
         }
         tokens.next();
     }
     if objects.is_empty() {
-        return Ok(Object::Void);
+        return Ok(Literal::Void);
     }
-    return Ok(Object::List(objects));
+    return Ok(Literal::List(objects));
 }
--- tests/parser_tests.rs
+++ tests/parser_tests.rs
@@ -4,21 +4,18 @@ use liwb::parser::*;
 #[test]
 fn empty_source_returns_void_object() {
     let source = "";
-    assert_eq!(
-        parse(lexer(source)).unwrap(),
-        Object::Void
-    );
+    assert_eq!(parse(lexer(source)).unwrap()[0], Literal::Void);
 }
 
 #[test]
 fn basic_arithmetic_operation() {
     let source = "(+ 1 2)";
     assert_eq!(
-        parse(lexer(source)).unwrap(),
-        Object::List(vec![
-            Object::BinaryOperator(Operator::Add),
-            Object::Number(1.0),
-            Object::Number(2.0),
+        parse(lexer(source)).unwrap()[0],
+        Literal::List(vec![
+            Literal::BinaryOperator(Operator::Add),
+            Literal::Number(1.0),
+            Literal::Number(2.0),
         ])
     );
 }
@@ -27,20 +24,20 @@ fn basic_arithmetic_operation() {
 fn nested() {
     let source = "(+ 1 (+ 2 (+ 3 (+ 4 5))))";
     assert_eq!(
-        parse(lexer(source)).unwrap(),
-        Object::List(vec![
-            Object::BinaryOperator(Operator::Add),
-            Object::Number(1.0),
-            Object::List(vec![
-                Object::BinaryOperator(Operator::Add),
-                Object::Number(2.0),
-                Object::List(vec![
-                    Object::BinaryOperator(Operator::Add),
-                    Object::Number(3.0),
-                    Object::List(vec![
-                        Object::BinaryOperator(Operator::Add),
-                        Object::Number(4.0),
-                        Object::Number(5.0),
+        parse(lexer(source)).unwrap()[0],
+        Literal::List(vec![
+            Literal::BinaryOperator(Operator::Add),
+            Literal::Number(1.0),
+            Literal::List(vec![
+                Literal::BinaryOperator(Operator::Add),
+                Literal::Number(2.0),
+                Literal::List(vec![
+                    Literal::BinaryOperator(Operator::Add),
+                    Literal::Number(3.0),
+                    Literal::List(vec![
+                        Literal::BinaryOperator(Operator::Add),
+                        Literal::Number(4.0),
+                        Literal::Number(5.0),
                     ])
                 ])
             ])
@@ -52,11 +49,11 @@ fn nested() {
 fn arithmetic_operation_with_void_on_right() {
     let source = "(+ 1 ())";
     assert_eq!(
-        parse(lexer(source)).unwrap(),
-        Object::List(vec![
-            Object::BinaryOperator(Operator::Add),
-            Object::Number(1.0),
-            Object::Void
+        parse(lexer(source)).unwrap()[0],
+        Literal::List(vec![
+            Literal::BinaryOperator(Operator::Add),
+            Literal::Number(1.0),
+            Literal::Void
         ])
     );
 }
@@ -65,31 +62,85 @@ fn arithmetic_operation_with_void_on_right() {
 fn arithmetic_operation_with_list_on_both_side() {
     let source = "(+ (+ 1 (* 2 3)) (* (/ 4 5) (- 6 7)))";
     assert_eq!(
-        parse(lexer(source)).unwrap(),
-        Object::List(vec![
-            Object::BinaryOperator(Operator::Add),
-            Object::List(vec![
-                Object::BinaryOperator(Operator::Add),
-                Object::Number(1.0),
-                Object::List(vec![
-                    Object::BinaryOperator(Operator::Multiply),
-                    Object::Number(2.0),
-                    Object::Number(3.0),
+        parse(lexer(source)).unwrap()[0],
+        Literal::List(vec![
+            Literal::BinaryOperator(Operator::Add),
+            Literal::List(vec![
+                Literal::BinaryOperator(Operator::Add),
+                Literal::Number(1.0),
+                Literal::List(vec![
+                    Literal::BinaryOperator(Operator::Multiply),
+                    Literal::Number(2.0),
+                    Literal::Number(3.0),
                 ])
             ]),
-            Object::List(vec![
-                Object::BinaryOperator(Operator::Multiply),
-                Object::List(vec![
-                    Object::BinaryOperator(Operator::Divide),
-                    Object::Number(4.0),
-                    Object::Number(5.0),
+            Literal::List(vec![
+                Literal::BinaryOperator(Operator::Multiply),
+                Literal::List(vec![
+                    Literal::BinaryOperator(Operator::Divide),
+                    Literal::Number(4.0),
+                    Literal::Number(5.0),
                 ]),
-                Object::List(vec![
-                    Object::BinaryOperator(Operator::Subtract),
-                    Object::Number(6.0),
-                    Object::Number(7.0),
+                Literal::List(vec![
+                    Literal::BinaryOperator(Operator::Subtract),
+                    Literal::Number(6.0),
+                    Literal::Number(7.0),
                 ]),
             ]),
         ])
+    );
+}
+
+#[test]
+fn define_variable() {
+    let source = "(define pi (/ 22 7))";
+    assert_eq!(
+        parse(lexer(source)).unwrap()[0],
+        Literal::List(vec![
+            Literal::Symbol("define".to_string()),
+            Literal::Symbol("pi".to_string()),
+            Literal::List(vec![
+                Literal::BinaryOperator(Operator::Divide),
+                Literal::Number(22.0),
+                Literal::Number(7.0),
+            ])
+        ]),
+    )
+}
+
+#[test]
+fn multi_line_statement() {
+    let source = "(define pi (/ 22 7))\n(define r 10)\n(define area-of-circle (* pi (* r r)))";
+    assert_eq!(
+        parse(lexer(source)).unwrap(),
+        vec![
+            Literal::List(vec![
+                Literal::Symbol("define".to_string()),
+                Literal::Symbol("pi".to_string()),
+                Literal::List(vec![
+                    Literal::BinaryOperator(Operator::Divide),
+                    Literal::Number(22.0),
+                    Literal::Number(7.0),
+                ])
+            ]),
+            Literal::List(vec![
+                Literal::Symbol("define".to_string()),
+                Literal::Symbol("r".to_string()),
+                Literal::Number(10.0)
+            ]),
+            Literal::List(vec![
+                Literal::Symbol("define".to_string()),
+                Literal::Symbol("area-of-circle".to_string()),
+                Literal::List(vec![
+                    Literal::BinaryOperator(Operator::Multiply),
+                    Literal::Symbol("pi".to_string()),
+                    Literal::List(vec![
+                        Literal::BinaryOperator(Operator::Multiply),
+                        Literal::Symbol("r".to_string()),
+                        Literal::Symbol("r".to_string()),
+                    ])
+                ])
+            ]),
+        ]
     );
 }
