542a03c84f6545b8056f05c3a9d7bcbee06987b8
CarlosDanielMaturano <carlosdanielmaturano@proton.me>
2024-05-16 10:56:02 -0300

    feat: Add function parsing and evaluation

---
+++ src/functions.rs
@@ -0,0 +1,93 @@
+use std::collections::HashMap;
+
+use crate::evaluator::*;
+use crate::literals::*;
+
+pub fn define_function(list: Vec<Literal>, variables: &mut Variables) -> Result<Literal, String> {
+    let mut list = list.into_iter().skip(1);
+    let name = list
+        .next()
+        .ok_or(format!("Error. Could not get the function name"))?;
+
+    let Literal::Symbol(name) = name else {
+        return Err(format!(
+            "Error. Expected Literal::Symbol to be function name, found: {:?}",
+            name
+        ));
+    };
+
+    let args = list
+        .next()
+        .ok_or(format!("Error. Could not get the function argument name"))?;
+
+    let Literal::Vector(args) = args else {
+        return Err(format!(
+            "Error. Expected Literal::Symbol to be function argument name, found: {:?}",
+            args
+        ));
+    };
+
+    let args = args
+        .into_iter()
+        .filter(|literal| *literal != Literal::Void)
+        .map(|arg| match arg {
+            Literal::Symbol(s) => Ok(s),
+            _ => Err(format!(
+                "Error. Expected Literal::Symbol type for argument name, found: {:?}",
+                arg
+            )),
+        })
+        .collect::<Result<Vec<_>, String>>()?;
+
+    let body = list
+        .next()
+        .ok_or(format!("Error. Could not get function's body."))?;
+
+    let Literal::List(body) = body else {
+        return Err(format!(
+            "Error. Expected function's body to be of type Literal::List, found: {:?}",
+            body
+        ));
+    };
+
+    let function = Literal::Function {
+        name: name.clone(),
+        args,
+        body: Box::new(Literal::List(body)),
+    };
+
+    variables.insert(name, function);
+    return Ok(Literal::Void);
+}
+
+pub fn eval_function(
+    function: Literal,
+    list: Vec<Literal>,
+    variables: &mut Variables,
+) -> Result<Literal, String> {
+    let values = list
+        .into_iter()
+        .map(|literal| eval_literal(literal, variables))
+        .skip(1)
+        .collect::<Result<Vec<_>, String>>()?;
+
+    let Literal::Function { args, body, .. } = function else {
+        return Err(format!("Error. Expected function, found: {:?}", function));
+    };
+
+    if values.len() != args.len() {
+        return Err(format!(
+            "Error. The number of arguments that the funciton requires differ from the passed"
+        ));
+    }
+
+    let mut local_variables: Variables = variables.clone();
+
+    args.into_iter()
+        .zip(values.into_iter())
+        .for_each(|(key, value)| {
+            local_variables.insert(key, value);
+        });
+
+    eval_literal(*body, &mut local_variables)
+}
---
+++ src/literals.rs
@@ -0,0 +1,31 @@
+#[derive(Debug, PartialEq, Clone)]
+pub enum MathOperators {
+    Add,
+    Subtract,
+    Multiply,
+    Divide,
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub enum Operator {
+    Equal,
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub enum Literal {
+    Void,
+    List(Vec<Literal>),
+    Vector(Vec<Literal>),
+    Number(f64),
+    Symbol(String),
+    String(String),
+    MathOperator(MathOperators),
+    Boolean(bool),
+    BinaryOperator(Operator),
+    If,
+    Function {
+        name: String,
+        args: Vec<String>,
+        body: Box<Literal>,
+    },
+}
